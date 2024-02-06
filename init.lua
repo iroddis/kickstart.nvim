816172b09671f14e4004ea50248ac7223148feb9
Ian Roddis <31021769+iroddis@users.noreply.github.com>
2024-02-06 16:14:22 -0400

    Removing vim-sleuth

--- init.lua
+++ init.lua
@@ -77,7 +77,7 @@ require('lazy').setup({
   'github/copilot.vim',
 
   -- Detect tabstop and shiftwidth automatically
-  'tpope/vim-sleuth',
+  -- 'tpope/vim-sleuth',
 
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
