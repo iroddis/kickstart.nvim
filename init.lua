84ea5aef3cbcfd6a98df392ecab0aaa4e1b94fd7
Ian Roddis <31021769+iroddis@users.noreply.github.com>
2024-09-12 08:56:47 -0300

    Adding better marks manager

--- init.lua
+++ init.lua
@@ -298,6 +298,13 @@ require('lazy').setup({
     build = ':TSUpdate',
   },
 
+  -- Marks management
+  {
+    "chentoast/marks.nvim",
+    event = "VeryLazy",
+    opts = {},
+  },
+
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
